c120adce1c544cb26104fb4b92873f932cce084d
fschwiet <fschwiet@gmail.com>
2011-12-28 22:47:10 -0800

    a test

---
+++ Namecheap.Tests/Can_set_a_hosts_entry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Namecheap.Util;
+using NJasmine;
+
+namespace Namecheap.Tests
+{
+    public class Can_set_a_hosts_entry : GivenWhenThenFixture
+    {
+        public override void Specify()
+        {
+            var domainName =
+                String.Format("foo{0}.{1}", Guid.NewGuid().ToString().Replace("-", ""),
+                    Properties.Settings.Default.TestBaseDomain);
+
+            var namecheapClient = new NamecheapClient(Properties.Settings.Default.APIKey);
+
+            when("we assign a hostname", delegate()
+            {
+                var firstIPAddress = "192.168.0.1";
+
+                arrange(() => namecheapClient.SetHostEntry(domainName, firstIPAddress));
+
+                then_hostname_has_IPAddress(namecheapClient, domainName, firstIPAddress);
+
+                when("we re-assign the hostname", delegate()
+                {
+                    var secondIPAddress = "192.168.0.2";
+
+                    arrange(() => namecheapClient.SetHostEntry(domainName, secondIPAddress));
+
+                    then_hostname_has_IPAddress(namecheapClient, domainName, secondIPAddress);
+                });
+            });
+        }
+
+        private void then_hostname_has_IPAddress(NamecheapClient namecheapClient, string domainName, string IPAddress)
+        {
+            then("it has the new value", delegate()
+            {
+                expect(() => namecheapClient.GetHostEntry(domainName) == IPAddress);
+            });
+        }
+    }
+}
---
+++ Namecheap.Util/NamecheapClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Namecheap.Util
+{
+    public class NamecheapClient
+    {
+        private readonly string _apiKey;
+
+        public NamecheapClient(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public void SetHostEntry(string hostname, string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetHostEntry(string domainName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
